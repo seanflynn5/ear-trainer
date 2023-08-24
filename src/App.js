03cdaf3c3979700585ee8eebb201fac1134fcad6
seanflynn5 <seanflynn5000@yahoo.com>
2023-08-24 12:25:50 -0400

    initial logic

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  
+  const major = [['C','E','G']];
+  const minor = [['C', 'Eb', 'G']];
+  const diminished = [['C', 'Eb', 'Gb']];
+  const [isMajor, setIsMajor] = useState(false);
+  const [isMinor, setIsMinor] = useState(false);
+  const [isDiminished, setIsDiminished] = useState(false);
+  const [selections, setSelections] = useState({
+    'major': true,
+    'minor': true,
+    'diminished': false
+  })
+
+  const handleDivClick = (type) => {
+    setSelections(prevSelections => ({
+      ...prevSelections,
+      [type]: !prevSelections[type]
+    }));
+  };
+
   return (
     <div className="App">
       <h1>My Ear Trainer</h1>
+      <div>
+      <div onClick={() => handleDivClick('major')}>
+        Major {selections.major ? 'Selected' : 'Not Selected'}
+      </div>
+      <div onClick={() => handleDivClick('minor')}>
+        Minor {selections.minor ? 'Selected' : 'Not Selected'}
+      </div>
+      <div onClick={() => handleDivClick('diminished')}>
+        Diminished {selections.diminished ? 'Selected' : 'Not Selected'}
+      </div>
+    </div>
     </div>
   );
 }
