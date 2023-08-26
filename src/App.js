b6282eb2bdb8222701f04d355aa792f5a4b75dbf
seanflynn5 <seanflynn5000@yahoo.com>
2023-08-26 10:58:57 -0400

    add essential formatting and functionality

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import './App.css';
 function App() {
   const [chords, setChords] = useState('');
   const [feedback, setFeedback] = useState('');
+  const [score, setScore] = useState(0);
   const major = [['C','E','G'], ['Db','F','Ab'], ['D','Gb','A'],['Eb','G','Bb'],['E','Ab','B'],['F','A','C'],['Gb','Bb','Db'],['G','B','D'],['Ab','C','Eb'],['A','Db','E'],['Bb','D','F'],['B','Eb','Gb']];
   const minor = [['C', 'Eb', 'G'],['Db','E','Ab'], ['D','F','A'],['Eb','Gb','Bb'],['E','A','B'],['F','Ab','C'],['Gb','A','Db'],['G','Bb','D'],['Ab','B','Eb'],['A','C','E'],['Bb','Db','F'],['B','D','Gb']];
   const diminished = [['C', 'Eb', 'Gb'],['Db','E','A'], ['D','F','Ab'],['Eb','Gb','A'],['E','A','Bb'],['F','Ab','B'],['Gb','A','C'],['G','Bb','Db'],['Ab','B','D'],['A','C','Eb'],['Bb','Db','E'],['B','D','F']];
+  const augmented = [['C','E','Ab'], ['Db','F','A'], ['D','Gb','Bb'],['Eb','G','B'],['E','Ab','C'],['F','A','Db'],['Gb','Bb','D'],['G','B','Eb'],['Ab','C','E'],['A','Db','F'],['Bb','D','Gb'],['B','Eb','G']];
   const [selections, setSelections] = useState({
     'major': true,
     'minor': true,
     'diminished': false
   })
   let randomChord;
+  let now;
   let octave;
   let synth0;
   let synth1;
@@ -45,21 +48,23 @@ function App() {
           newChords.push(diminished[i])
         }
       }
+      if (selections[key] && key === 'augmented') {
+        for (let i = 0; i < augmented.length; i++) {
+          newChords.push(augmented[i])
+        }
+      }
     }
     console.log(newChords)
-    setChords(newChords)
+    setChords(newChords);
   }  
 
   const playChord = () => {
-    console.log('chords', chords)
     randomChord = chords[Math.floor(Math.random() * chords.length)]
-    console.log('item', randomChord)
-    octave = Math.floor(Math.random() * 3) + 3;
-    console.log('octave', octave);
+    octave = Math.random() < 0.5 ? 4 : 5;
     synth0 = new Tone.Synth().toDestination();
     synth1 = new Tone.Synth().toDestination();
     synth2 = new Tone.Synth().toDestination();
-    const now = Tone.now();
+    now = Tone.now();
     synth0.triggerAttack(randomChord[0] + octave, now);
     synth0.triggerRelease(now + 1);
     synth1.triggerAttack(randomChord[1] + octave, now);
@@ -69,11 +74,13 @@ function App() {
   }
 
   const repeatChord = () => {
+    console.log('randomChord', randomChord)
     if (!randomChord) {
+      console.log('here')
       setFeedback('Play a chord!');
       return;
     }
-    const now = Tone.now();
+    now = Tone.now();
     synth0.triggerAttack(randomChord[0] + octave, now);
     synth0.triggerRelease(now + 1);
     synth1.triggerAttack(randomChord[1] + octave, now);
@@ -91,13 +98,15 @@ function App() {
     if (
       (choice === 'Major' && containsArray(major, randomChord)) ||
       (choice === 'Minor' && containsArray(minor, randomChord)) ||
-      (choice === 'Diminished' && containsArray(diminished, randomChord))
+      (choice === 'Diminished' && containsArray(diminished, randomChord)) ||
+      (choice === 'Augmented' && containsArray(augmented, randomChord))
     ) {
       setFeedback('Right!');
+      setScore(score + 1);
     } else {
       setFeedback('Wrong!');
+      setScore(0);
     }
-    console.log('randomChord', randomChord)
   }
 
   function containsArray(arrayOfArrays, targetArray) {
@@ -106,7 +115,6 @@ function App() {
         return true;
       }
     }
-    console.log('here')
     return false;
   }
 
@@ -116,40 +124,55 @@ function App() {
 
 return (
   <div className='App'>
-    <h1 className="display-1">My Ear Trainer</h1>
-    <div className="container mt-5 d-flex justify-content-center">
-    <h2>Chord Types:</h2>
-      <div className="d-flex justify-content-center">
+    <h1 className="display-4">My Ear Trainer</h1>
+    <div className="container mt-4 d-flex justify-content-center">
+      <h2 className="h4">Chord Types:</h2>
+      <div className="d-flex justify-content-center row">
         <button
-          className={`btn ${selections.major ? 'btn-success' : 'btn-secondary'} mx-2`}
+          className={`col-6 my-1 btn btn-sm ${selections.major ? 'btn-success' : 'btn-secondary'} mx-2`}
           onClick={() => handleDivClick('major')}
         >
           Major
         </button>
         <button
-          className={`btn ${selections.minor ? 'btn-success' : 'btn-secondary'} mx-2`}
+          className={`col-6 my-1 btn btn-sm ${selections.minor ? 'btn-success' : 'btn-secondary'} mx-2`}
           onClick={() => handleDivClick('minor')}
         >
           Minor
         </button>
         <button
-          className={`btn ${selections.diminished ? 'btn-success' : 'btn-secondary'} mx-2`}
+          className={`col-6 my-1 btn btn-sm ${selections.diminished ? 'btn-success' : 'btn-secondary'} mx-2`}
           onClick={() => handleDivClick('diminished')}
         >
           Diminished
         </button>
+        <button
+          className={`col-6 my-1 btn btn-sm ${selections.augmented ? 'btn-success' : 'btn-secondary'} mx-2`}
+          onClick={() => handleDivClick('augmented')}
+        >
+          Augmented
+        </button>
       </div>
     </div>
-    <button className="btn btn-dark mt-3" onClick={() => playChord()}>Play Chord</button>
-    <button className="btn btn-dark mt-3" onClick={() => repeatChord()}>Repeat Chord</button>
-    <div className="d-flex justify-content-center mt-3">
-      <button className="btn btn-secondary mx-2" onClick={(e) => checkChord(e)}>Major</button>
-      <button className="btn btn-secondary mx-2" onClick={(e) => checkChord(e)}>Minor</button>
-      <button className="btn btn-secondary mx-2" onClick={(e) => checkChord(e)}>Diminished</button>
+    <div className="container mt-4 d-flex justify-content-center">
+      <button className="btn btn-dark mt-2 mx-1 btn-sm" onClick={() => playChord()}>Play Chord</button>
+      <button className="btn btn-dark mt-2 mx-1 btn-sm" onClick={() => repeatChord()}>Repeat Chord</button>
     </div>
-    <div className="mt-3">
-        <p className={feedback === 'Right!' ? 'text-success' : 'text-danger'}>{feedback}</p>
+    <div className="container mt-4 d-flex justify-content-center">
+      <h2 className="h4">Selection: </h2>
+      <div className="d-flex justify-content-center row">
+        <button className="col-6 btn btn-secondary mx-2 my-1 btn-sm" onClick={(e) => checkChord(e)}>Major</button>
+        <button className="col-6 btn btn-secondary mx-2 my-1 btn-sm" onClick={(e) => checkChord(e)}>Minor</button>
+        <button className="col-6 btn btn-secondary mx-2 my-1 btn-sm" onClick={(e) => checkChord(e)}>Diminished</button>
+        <button className="col-6 btn btn-secondary mx-2 my-1 btn-sm" onClick={(e) => checkChord(e)}>Augmented</button>
       </div>
+    </div>
+    <div className="mt-2">
+      <p className={feedback === 'Right!' ? 'text-success' : 'text-danger'}>{feedback}</p>
+    </div>
+    <div className="container mt-4 d-flex justify-content-center">
+      <h2 className="h4">Score: {score}</h2>
+    </div>
   </div>
 );
 }
