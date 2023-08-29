bab4bb8307e8d66c95ac746bba16a979294b0cb1
seanflynn5 <seanflynn5000@yahoo.com>
2023-08-29 11:29:03 -0400

    remove tour

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import * as Tone from 'tone';
-import Tour from './Tour';
 import { FaPlay, FaRedo } from 'react-icons/fa';
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
